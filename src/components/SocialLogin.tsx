92857b3369aeef52bc016ff21b522c6b656f5cda
니꼬 <nico@nomadcoders.co>
2022-09-23 18:22:07 +0900

    20.5 Github Log In

--- src/components/SocialLogin.tsx
+++ src/components/SocialLogin.tsx
@@ -12,7 +12,12 @@ export default function SocialLogin() {
         <Divider />
       </HStack>
       <VStack>
-        <Button w="100%" leftIcon={<FaGithub />}>
+        <Button
+          as="a"
+          href="https://github.com/login/oauth/authorize?client_id=5195598d392601f20eea&scope=read:user,user:email"
+          w="100%"
+          leftIcon={<FaGithub />}
+        >
           Continue with Github
         </Button>
         <Button w="100%" leftIcon={<FaComment />} colorScheme={"yellow"}>

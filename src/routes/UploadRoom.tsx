0ba1028c96249fddf3f3b4f2ceab142f79a343f8
니꼬 <nico@nomadcoders.co>
2022-09-27 13:23:31 +0900

    21.1 Upload Form

--- src/routes/UploadRoom.tsx
+++ src/routes/UploadRoom.tsx
@@ -1,11 +1,109 @@
+import {
+  Box,
+  Checkbox,
+  Container,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Heading,
+  Input,
+  InputGroup,
+  InputLeftAddon,
+  InputLeftElement,
+  NumberDecrementStepper,
+  NumberIncrementStepper,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  Select,
+  Textarea,
+  VStack,
+} from "@chakra-ui/react";
+import { FaBed, FaMoneyBill, FaToilet, FaUsb } from "react-icons/fa";
 import useHostOnlyPage from "../components/HostOnlyPage";
 import ProtectedPage from "../components/ProtectedPage";
+
+/*
+kind
+---
+amenities
+category
+*/
 
 export default function UploadRoom() {
   useHostOnlyPage();
   return (
     <ProtectedPage>
-      <h1>upload roommmmmmm</h1>
+      <Box
+        pb={40}
+        mt={10}
+        px={{
+          base: 10,
+          lg: 40,
+        }}
+      >
+        <Container>
+          <Heading textAlign={"center"}>Upload Room</Heading>
+          <VStack spacing={5} as="form" mt={5}>
+            <FormControl>
+              <FormLabel>Name</FormLabel>
+              <Input required type="text" />
+              <FormHelperText>Write the name of your room.</FormHelperText>
+            </FormControl>
+            <FormControl>
+              <FormLabel>Country</FormLabel>
+              <Input required type="text" />
+            </FormControl>
+            <FormControl>
+              <FormLabel>City</FormLabel>
+              <Input required type="text" />
+            </FormControl>
+            <FormControl>
+              <FormLabel>Address</FormLabel>
+              <Input required type="text" />
+            </FormControl>
+            <FormControl>
+              <FormLabel>Price</FormLabel>
+              <InputGroup>
+                <InputLeftAddon children={<FaMoneyBill />} />
+                <Input type="number" min={0} />
+              </InputGroup>
+            </FormControl>
+            <FormControl>
+              <FormLabel>Rooms</FormLabel>
+              <InputGroup>
+                <InputLeftAddon children={<FaBed />} />
+                <Input type="number" min={0} />
+              </InputGroup>
+            </FormControl>
+            <FormControl>
+              <FormLabel>Toilets</FormLabel>
+              <InputGroup>
+                <InputLeftAddon children={<FaToilet />} />
+                <Input type="number" min={0} />
+              </InputGroup>
+            </FormControl>
+            <FormControl>
+              <FormLabel>Description</FormLabel>
+              <Textarea />
+            </FormControl>
+            <FormControl>
+              <Checkbox>Pet friendly?</Checkbox>
+            </FormControl>
+            <FormControl>
+              <FormLabel>Kind of room</FormLabel>
+              <Select placeholder="Choose a kind">
+                <option value="entire_place">Entire Place</option>
+                <option value="private_room">Private Room</option>
+                <option value="shared_room">Shared Room</option>
+              </Select>
+              <FormHelperText>
+                What kind of room are you renting?
+              </FormHelperText>
+            </FormControl>
+          </VStack>
+        </Container>
+      </Box>
     </ProtectedPage>
   );
 }

54ed405de878c849ca2383e488b2e3fefe8435c9
니꼬 <nico@nomadcoders.co>
2022-09-28 17:10:48 +0900

    goToRangeStartOnSelect

--- src/routes/RoomDetail.tsx
+++ src/routes/RoomDetail.tsx
@@ -148,7 +148,6 @@ export default function RoomDetail() {
         </Box>
         <Box pt={10}>
           <Calendar
-            goToRangeStartOnSelect
             onChange={setDates}
             prev2Label={null}
             next2Label={null}

318470958f7383bbb4aa3189cad6016201abcf2d
니꼬 <nico@nomadcoders.co>
2022-09-28 18:16:00 +0900

    https://airbnbclonetest.xyz/

--- src/api.ts
+++ src/api.ts
@@ -7,7 +7,7 @@ const instance = axios.create({
   baseURL:
     process.env.NODE_ENV === "development"
       ? "http://127.0.0.1:8000/api/v1/"
-      : "https://backend.airbnbclonetest.xyz/api/v1",
+      : "https://backend.https://airbnbclonetest.xyz/api/v1/",
   withCredentials: true,
 });
 
